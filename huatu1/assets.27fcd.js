3e2da89d976df755cdcaafe7bb29fc66d7cd211b
xuyanfeng <xu_yanfeng@126.com>
2020-04-24 17:34:54 +0800

    no message

---
+++ huatu1/assets.27fcd.js
@@ -0,0 +1,330 @@
+window.__AvgAssets={
+    "5bd433c6-ab0c-443f-9374-e19d68c08f00": {
+        "path": "res/default/avg-menus.png",
+        "type": "image",
+        "md5": "res/avg/5b/5bd433c6-ab0c-443f-9374-e19d68c08f00.2dd41.png"
+    },
+    "80683f67-fb22-4b5f-a157-e86f58fe3518": {
+        "path": "res/default/ui-file-mgr/back.png",
+        "type": "image",
+        "md5": "res/avg/80/80683f67-fb22-4b5f-a157-e86f58fe3518.baef2.png"
+    },
+    "fcd183b7-5e0c-4203-8f15-2c619df86c54": {
+        "path": "res/default/cover-bg.png",
+        "type": "image",
+        "md5": "res/avg/fc/fcd183b7-5e0c-4203-8f15-2c619df86c54.8eb08.png"
+    },
+    "89550671-cc19-4117-854c-ec319787110c": {
+        "path": "res/default/btn-press.png",
+        "type": "image",
+        "md5": "res/avg/89/89550671-cc19-4117-854c-ec319787110c.2dc54.png"
+    },
+    "3dbaa718-5e90-477d-ba66-e98b509c8887": {
+        "path": "res/default/ui-file-mgr/frame-normal.png",
+        "type": "image",
+        "md5": "res/avg/3d/3dbaa718-5e90-477d-ba66-e98b509c8887.796ea.png"
+    },
+    "d9a576f1-8f3b-484f-8758-989abc3132a0": {
+        "path": "res/default/ui-file-mgr/icon.png",
+        "type": "image",
+        "md5": "res/avg/d9/d9a576f1-8f3b-484f-8758-989abc3132a0.f482b.png"
+    },
+    "471d56a8-c474-41cd-af5a-030ee3944b7b": {
+        "path": "res/default/ui-env/frame.png",
+        "type": "image",
+        "md5": "res/avg/47/471d56a8-c474-41cd-af5a-030ee3944b7b.c4c62.png"
+    },
+    "6712c1aa-05e3-453a-9473-c62350aab775": {
+        "path": "res/default/ui-env/slider1.png",
+        "type": "image",
+        "md5": "res/avg/67/6712c1aa-05e3-453a-9473-c62350aab775.5cf64.png"
+    },
+    "6da73290-5995-44b3-8507-3ce2796869ee": {
+        "path": "res/default/ui-env/slider2.png",
+        "type": "image",
+        "md5": "res/avg/6d/6da73290-5995-44b3-8507-3ce2796869ee.eafd4.png"
+    },
+    "9d68a76f-d6e5-495c-9d7e-066c0b143409": {
+        "path": "res/default/ui-env/button.png",
+        "type": "image",
+        "md5": "res/avg/9d/9d68a76f-d6e5-495c-9d7e-066c0b143409.d0c43.png"
+    },
+    "adebbcd5-7832-4761-a0a3-f30303ea7d42": {
+        "path": "cfg/init.json",
+        "type": "config",
+        "md5": "res/avg/ad/adebbcd5-7832-4761-a0a3-f30303ea7d42.e4abc.json"
+    },
+    "5f2efc47-59c9-41f7-bb1f-57d6a2d31677": {
+        "path": "cfg/settings.json",
+        "type": "config",
+        "md5": "res/avg/5f/5f2efc47-59c9-41f7-bb1f-57d6a2d31677.41e09.json"
+    },
+    "e2478c4d-3242-4eb0-9719-88ef5061e6ba": {
+        "path": "cfg/ui.json",
+        "type": "config",
+        "md5": "res/avg/e2/e2478c4d-3242-4eb0-9719-88ef5061e6ba.9a786.json"
+    },
+    "4d2ebe85-7d75-4708-9ef1-dbcac0e4bad6": {
+        "path": "res/default/btn-end.png",
+        "type": "image",
+        "md5": "res/avg/4d/4d2ebe85-7d75-4708-9ef1-dbcac0e4bad6.266d5.png"
+    },
+    "c8f8d45e-01f2-4e9a-b07d-9d010c2477f5": {
+        "path": "res/default/btn-replay.png",
+        "type": "image",
+        "md5": "res/avg/c8/c8f8d45e-01f2-4e9a-b07d-9d010c2477f5.6ba06.png"
+    },
+    "7765d11f-9a69-4420-baf1-b72435a5a39d": {
+        "path": "res/default/bag/avg-bag.png",
+        "type": "image",
+        "md5": "res/avg/77/7765d11f-9a69-4420-baf1-b72435a5a39d.c8402.png"
+    },
+    "326cc327-6137-47db-ad2f-5d4db14fcc34": {
+        "path": "res/default/bag/back.png",
+        "type": "image",
+        "md5": "res/avg/32/326cc327-6137-47db-ad2f-5d4db14fcc34.0760d.png"
+    },
+    "ae97375b-12bb-4b5c-8492-62319eea6b53": {
+        "path": "res/default/bag/bg.png",
+        "type": "image",
+        "md5": "res/avg/ae/ae97375b-12bb-4b5c-8492-62319eea6b53.3b7ec.png"
+    },
+    "6762f7c0-0ce5-4b26-a840-a7204a85e3c6": {
+        "path": "cfg/ui/cover.json",
+        "type": "config",
+        "md5": "res/avg/67/6762f7c0-0ce5-4b26-a840-a7204a85e3c6.695a3.json"
+    },
+    "cea00c35-6d50-488d-af7b-80a7951db101": {
+        "path": "cfg/ui/file-management.json",
+        "type": "config",
+        "md5": "res/avg/ce/cea00c35-6d50-488d-af7b-80a7951db101.3920a.json"
+    },
+    "6687c830-2efe-4922-8a7d-4a8bb1e82a01": {
+        "path": "cfg/ui/environment.json",
+        "type": "config",
+        "md5": "res/avg/66/6687c830-2efe-4922-8a7d-4a8bb1e82a01.eff0d.json"
+    },
+    "0dd627c2-8baf-4f15-88ac-9a4198489579": {
+        "path": "cfg/ui/menu.json",
+        "type": "config",
+        "md5": "res/avg/0d/0dd627c2-8baf-4f15-88ac-9a4198489579.3e782.json"
+    },
+    "adb9d2da-52c5-468e-b420-474550a5c8b2": {
+        "path": "cfg/ui/entry-set.json",
+        "type": "config",
+        "md5": "res/avg/ad/adb9d2da-52c5-468e-b420-474550a5c8b2.d9a6e.json"
+    },
+    "2508e143-0e3a-49b5-aeaa-cea0ea3cd3fd": {
+        "path": "cfg/piece.json",
+        "type": "config",
+        "md5": "res/avg/25/2508e143-0e3a-49b5-aeaa-cea0ea3cd3fd.32f0d.json"
+    },
+    "b1961f53-2c03-4a5d-8ba5-75a6322eb9f5": {
+        "path": "cfg/plot.json",
+        "type": "config",
+        "md5": "res/avg/b1/b1961f53-2c03-4a5d-8ba5-75a6322eb9f5.4bd69.json"
+    },
+    "3af47cc3-e51c-4769-bd3c-3329bcf0891b": {
+        "path": "cfg/role-scene.json",
+        "type": "config",
+        "md5": "res/avg/3a/3af47cc3-e51c-4769-bd3c-3329bcf0891b.c3caf.json"
+    },
+    "22117c12-3b8a-412c-a65c-2762a1c0477a": {
+        "path": "cfg/numerical.json",
+        "type": "config",
+        "md5": "res/avg/22/22117c12-3b8a-412c-a65c-2762a1c0477a.8667e.json"
+    },
+    "1e36b2b0-546b-47bd-b52b-82cb324a1bc9": {
+        "path": "cfg/goods.json",
+        "type": "config",
+        "md5": "res/avg/1e/1e36b2b0-546b-47bd-b52b-82cb324a1bc9.36dec.json"
+    },
+    "3827037a-dacb-4ba9-ad8d-0411306878be": {
+        "path": "cfg/assets.json",
+        "type": "config",
+        "md5": "res/avg/38/3827037a-dacb-4ba9-ad8d-0411306878be.20ceb.json"
+    },
+    "0afa7b7a-7c46-4560-8aae-2169e3db7bc5": {
+        "path": "res/video/plot/1.mp4",
+        "type": "video",
+        "duration": 36.76,
+        "start": 0,
+        "md5": "res/avg/0a/0afa7b7a-7c46-4560-8aae-2169e3db7bc5.ed25d.mp4"
+    },
+    "19b9d989-56c6-4b06-aea5-a4e80f15ce8b": {
+        "path": "res/video/plot/2.mp4",
+        "type": "video",
+        "duration": 32.21,
+        "start": 0,
+        "md5": "res/avg/19/19b9d989-56c6-4b06-aea5-a4e80f15ce8b.6e4ec.mp4"
+    },
+    "0a760fab-4622-4acd-a65a-eddd8de6ff56": {
+        "path": "res/video/plot/4.mp4",
+        "type": "video",
+        "duration": 28.29,
+        "start": 0,
+        "md5": "res/avg/0a/0a760fab-4622-4acd-a65a-eddd8de6ff56.cfa05.mp4"
+    },
+    "06e4e9b4-6e8b-4214-9465-cfe31cd57a7d": {
+        "path": "res/audio/aside/1.mp3",
+        "type": "audio",
+        "duration": 33.15,
+        "md5": "res/avg/06/06e4e9b4-6e8b-4214-9465-cfe31cd57a7d.28cff.mp3"
+    },
+    "c0132a43-b3c2-44b8-ab0c-c15dd62fcd0a": {
+        "path": "res/default/bagItemDetails/bg.png",
+        "type": "image",
+        "md5": "res/avg/c0/c0132a43-b3c2-44b8-ab0c-c15dd62fcd0a.f80b8.png"
+    },
+    "1647054e-703f-439a-9784-6a5454059ddd": {
+        "path": "res/default/bagItemDetails/btnUse.png",
+        "type": "image",
+        "md5": "res/avg/16/1647054e-703f-439a-9784-6a5454059ddd.c5fe2.png"
+    },
+    "b802969f-b2c4-430b-95fd-5342e803a304": {
+        "path": "res/default/bagItemDetails/btnVideo.png",
+        "type": "image",
+        "md5": "res/avg/b8/b802969f-b2c4-430b-95fd-5342e803a304.183a0.png"
+    },
+    "78d2a00e-8b8b-4636-beae-f226f82dee32": {
+        "path": "res/default/bagItemGet/bg1.png",
+        "type": "image",
+        "md5": "res/avg/78/78d2a00e-8b8b-4636-beae-f226f82dee32.c8aca.png"
+    },
+    "f429074e-40dc-4b61-9c3c-346d3aa0103c": {
+        "path": "res/default/bagItemGet/bg2.png",
+        "type": "image",
+        "md5": "res/avg/f4/f429074e-40dc-4b61-9c3c-346d3aa0103c.77bdf.png"
+    },
+    "37995577-8786-44ca-ba4d-23d5e74822e3": {
+        "path": "res/default/bagItemGet/bg3.png",
+        "type": "image",
+        "md5": "res/avg/37/37995577-8786-44ca-ba4d-23d5e74822e3.3dec4.png"
+    },
+    "7e26068c-d470-4282-9265-937eff160aba": {
+        "path": "res/default/bagItemGet/btnJump.png",
+        "type": "image",
+        "md5": "res/avg/7e/7e26068c-d470-4282-9265-937eff160aba.4288c.png"
+    },
+    "b452d3b1-a88f-4b8f-94c6-bd904e5e549d": {
+        "path": "res/image/item/touming.png",
+        "type": "image",
+        "md5": "res/avg/b4/b452d3b1-a88f-4b8f-94c6-bd904e5e549d.325fc.png"
+    },
+    "6ff652d8-fcb3-4745-9914-dcba42ae1d6c": {
+        "path": "res/image/item/button0.png",
+        "type": "image",
+        "md5": "res/avg/6f/6ff652d8-fcb3-4745-9914-dcba42ae1d6c.ef487.png"
+    },
+    "beb7ebf4-7bf1-4dde-a432-23d4571120e5": {
+        "path": "res/image/item/button1.png",
+        "type": "image",
+        "md5": "res/avg/be/beb7ebf4-7bf1-4dde-a432-23d4571120e5.370df.png"
+    },
+    "8e1164b9-26b2-4886-92ac-d027ce2ba146": {
+        "path": "res/image/item/button2.png",
+        "type": "image",
+        "md5": "res/avg/8e/8e1164b9-26b2-4886-92ac-d027ce2ba146.79415.png"
+    },
+    "781dd540-9c1a-4f6b-8560-125f6c2010e1": {
+        "path": "res/image/item/button3.png",
+        "type": "image",
+        "md5": "res/avg/78/781dd540-9c1a-4f6b-8560-125f6c2010e1.9b1b8.png"
+    },
+    "93f247b8-b203-4ba7-aed4-2a27153c5af9": {
+        "path": "res/image/item/list1.png",
+        "type": "image",
+        "md5": "res/avg/93/93f247b8-b203-4ba7-aed4-2a27153c5af9.fc262.png"
+    },
+    "b09c8ae1-d068-46dd-9ea3-0666ee54615f": {
+        "path": "res/image/item/end.png",
+        "type": "image",
+        "md5": "res/avg/b0/b09c8ae1-d068-46dd-9ea3-0666ee54615f.258b6.png"
+    },
+    "1ae21312-533f-4c58-9858-d2e30d027f21": {
+        "path": "res/image/item/list2.png",
+        "type": "image",
+        "md5": "res/avg/1a/1ae21312-533f-4c58-9858-d2e30d027f21.d7d93.png"
+    },
+    "3e5751e2-e6ef-4eb3-9e7d-4ae084810413": {
+        "path": "res/video/plot/b.mp4",
+        "type": "video",
+        "duration": 59.29,
+        "start": 0,
+        "md5": "res/avg/3e/3e5751e2-e6ef-4eb3-9e7d-4ae084810413.fc1e7.mp4"
+    },
+    "8daf374b-1290-4aa2-bca3-4f3a5f202f2e": {
+        "path": "res/video/plot/a.mp4",
+        "type": "video",
+        "duration": 30.31,
+        "start": 0,
+        "md5": "res/avg/8d/8daf374b-1290-4aa2-bca3-4f3a5f202f2e.d097f.mp4"
+    },
+    "bf960883-8013-4c40-88d4-25160088ce30": {
+        "path": "res/image/item/真题-解题思路.jpg",
+        "type": "image",
+        "md5": "res/avg/bf/bf960883-8013-4c40-88d4-25160088ce30.6d994.jpg"
+    },
+    "da6491ca-9494-43e3-809a-8e8d980ab8ef": {
+        "path": "res/image/item/tingti.jpg",
+        "type": "image",
+        "md5": "res/avg/da/da6491ca-9494-43e3-809a-8e8d980ab8ef.611f3.jpg"
+    },
+    "fcd923bf-ab2b-4a2a-87cd-0cdc647ba006": {
+        "path": "res/image/bg/start.jpg",
+        "type": "image",
+        "md5": "res/avg/fc/fcd923bf-ab2b-4a2a-87cd-0cdc647ba006.1e14a.jpg"
+    },
+    "95e36348-5f1c-4d5a-b5fb-2bf959597b1e": {
+        "path": "res/image/bg/kuang.png",
+        "type": "image",
+        "md5": "res/avg/95/95e36348-5f1c-4d5a-b5fb-2bf959597b1e.f096a.png"
+    },
+    "60227ba7-0dee-404a-b56b-dede8ccaa26a": {
+        "path": "res/image/bg/start2.jpg",
+        "type": "image",
+        "md5": "res/avg/60/60227ba7-0dee-404a-b56b-dede8ccaa26a.58193.jpg"
+    },
+    "c373fd67-baf2-43c8-a9ae-fb606c45a4c4": {
+        "path": "res/image/bg/tou2.png",
+        "type": "image",
+        "md5": "res/avg/c3/c373fd67-baf2-43c8-a9ae-fb606c45a4c4.f096a.png"
+    },
+    "2ce53319-3cb4-4a2d-bd52-b53d15f65312": {
+        "path": "cfg/bag/itemList.json",
+        "type": "config",
+        "md5": "res/avg/2c/2ce53319-3cb4-4a2d-bd52-b53d15f65312.2fc62.json"
+    },
+    "2938b3c6-36ea-4b66-939c-b79a83c1f937": {
+        "path": "cfg/bag/itemDetails.json",
+        "type": "config",
+        "md5": "res/avg/29/2938b3c6-36ea-4b66-939c-b79a83c1f937.eb279.json"
+    },
+    "ccd313f9-17d7-4938-af6a-1d2ba685be98": {
+        "path": "cfg/bag/getItems1.json",
+        "type": "config",
+        "md5": "res/avg/cc/ccd313f9-17d7-4938-af6a-1d2ba685be98.41d10.json"
+    },
+    "126e0542-0794-4173-90ff-e96509ae8e74": {
+        "path": "cfg/bag/getItems2.json",
+        "type": "config",
+        "md5": "res/avg/12/126e0542-0794-4173-90ff-e96509ae8e74.32f69.json"
+    },
+    "6df7d649-d86c-4163-b9c8-21713a283c85": {
+        "path": "cfg/bag/getItems3.json",
+        "type": "config",
+        "md5": "res/avg/6d/6df7d649-d86c-4163-b9c8-21713a283c85.4c91e.json"
+    },
+    "28d0669a-f898-4bb0-9cd4-fff70c0ac818": {
+        "path": "cfg/scenes.json",
+        "md5": "res/avg/28/28d0669a-f898-4bb0-9cd4-fff70c0ac818.ef0f2.json"
+    },
+    "b668188c-da41-447c-b195-878fc40681ba": {
+        "path": "video/video.json",
+        "md5": "video/video.06275.json"
+    },
+    "708cb6f9-9968-41cf-b5d5-05909c64b351": {
+        "path": "video/video.mp4",
+        "md5": "video/video.90e85.mp4"
+    }
+}
